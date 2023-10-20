3af46a4661b2b1c3b39db1e0789aadb37656fe88
vipinwelcome <vipinkumarsharma800@gmail.com>
2023-10-20 12:49:20 +0530

    watching

--- practicing.js
+++ practicing.js
@@ -3,5 +3,5 @@ const array2 = new Array("Bittu", "Arvind");
 console.log(array1);
 array1[2]="Satish";
 console.log(array1.length);
-console.log(array1[1]);
+console.log(array1);
 console.log(array2[0]);
