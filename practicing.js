86606211e62f7e87c1781695764b13f1181589df
vipinwelcome <vipinkumarsharma800@gmail.com>
2023-12-13 14:10:43 +0530

    Conditions

--- practicing.js
+++ practicing.js
@@ -1,5 +1,14 @@
-const number = 4;
-if(number>=1) {
-    console.log("Pappu Passed");
+let a=1;
+switch(a){
+    case 1:a="Apple";
+    break;
+    case 2:a="Mango";
+    break;
+    case 3:a="Banana";
+    break;
+    case 4:a="Sandwitch";
+    break;
+    case 5:a="Dosa";
+    break;
 }
-console.log("Maza aa gaya"); 
+console.log(a);
