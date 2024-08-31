8ded50cc6b74bb3666257d19d1c2ea4200fdd944
vipinwelcome <vipinkumarsharma800@gmail.com>
2024-08-31 14:27:31 +0530

    Arrays input

--- practicing.js
+++ practicing.js
@@ -1,2 +1,6 @@
-let var=10;
-console.log(var);
+let students=[['Arun',24],['Seem',45]];
+students[1][2]='hello';
+console.log(students);
+students.splice(1,0,['Peter',24]);
+console.log(students);
+console.log(students[2]);
